592496f506a3f4dcc4b67d4ae9df1d5559467080
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-10-20 15:40:31 +0300

    upload files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,16 @@
-// import React from 'react';
-import React, { lazy, Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
-// import Form from './Form/Form';
-// import ContactList from './Contacts/ContactList';
-import Section from './Section/Section';
-// import Filter from './Filter/Filter';
-import Navigation from './Navigation/Navigation';
-
-const AddHero = lazy(() => import('./Form/Form'));
-const Heroes = lazy(() => import('./Contacts/ContactList'));
-
-export default function App() {
+export const App = () => {
   return (
-    <>
-      <Navigation />
-      <Section>
-        <Suspense fallback={<h1>Loading....</h1>}>
-          <Routes>
-            <Route path="/">
-              <Route path="/heroes" element={<Heroes />} />
-              <Route path="new-hero" element={<AddHero />} />
-            </Route>
-          </Routes>
-        </Suspense>
-      </Section>
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
-}
+};
